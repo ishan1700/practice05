525ee6d6a846ec3eaa23d7f74def79b018eda84a
Ishan Gurao <85431007+ishan1700@users.noreply.github.com>
2024-09-16 06:28:48 +0000

    Environment creation

--- EBS.tf
+++ EBS.tf
@@ -1,5 +1,12 @@
+#Application creation
 resource "aws_elastic_beanstalk_application" "healthApp" {
   name        = "healthApp"
   description = "Elastic Beanstalk Application."
 }
 
+#Environment creation
+resource "aws_elastic_beanstalk_environment" "healthApp_Environment" {
+  name                = "healthApp"
+  application         = aws_elastic_beanstalk_application.healthApp.name
+  solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4"
+}
